5d032b5b5b04a230748f61ac61e4690fb3a464e4
LoginALee <alejandrodxwwe@hotmail.com>
2020-01-15 13:27:21 -0600

    Tabla como componente

--- src/Components/users/index.js
+++ src/Components/users/index.js
@@ -3,6 +3,9 @@ import { connect } from "react-redux";
 import * as usuariosActions  from "../../actions/usuariosActions";
 import Spinner from "../general/Spinner";
 import  Fatal  from "../general/Fatal";
+import  Tabla  from "./Tabla";
+
+
 class Usuarios extends Component{
 
   componentDidMount(){
@@ -18,42 +21,16 @@ class Usuarios extends Component{
       return <Fatal mensaje={ this.props.error } />;
     }
 
-    return(
-      <table className='tabla'>
-          <thead>
-            <tr>
-              <th>Nombre</th>
-              <th>Correo</th>
-              <th>Enlace</th>
-            </tr>
-          </thead>
-          <tbody>
-            { this.ponerFilas() }
-          </tbody>
-        </table>
-    )
+    return <Tabla />
   }
 
 
-  ponerFilas = () => (
-    this.props.usuarios.map((usuario) =>(
-      <tr key={usuario.id}>
-        <td>
-          {usuario.name}
-        </td>
-        <td>
-          {usuario.email}
-        </td>
-        <td>
-          {usuario.website}
-        </td>
-      </tr>
-    ))
-  );
+  
 
   render(){
     return (
       <div>
+        <h1>Usuarios</h1>
         { this.ponerContenido() }
       </div>
     );
