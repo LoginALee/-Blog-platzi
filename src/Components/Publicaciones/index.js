cb6fbdd5808d6d47bb44308fb0d08a60f3f8860a
LoginALee <alejandrodxwwe@hotmail.com>
2020-01-18 12:34:33 -0600

    Estado con interaccion

--- src/Components/Publicaciones/index.js
+++ src/Components/Publicaciones/index.js
@@ -97,6 +97,7 @@ class Publicaciones extends Component{
                 onClick={ ()=> this.props.abrirCerrar(pub_key, com_key)}>
                 <h2>{ publicacion.title }</h2>
                 <h3>{ publicacion.body }</h3>
+                {(publicacion.abierto) ? 'abierto' : 'cerrado'}
             </div>
     )));
 
