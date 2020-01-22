e29871c77709b9a9753a11fc4a9948efb9b19864
LoginALee <alejandrodxwwe@hotmail.com>
2020-01-22 12:19:27 -0600

    Manejar inputs con reducer

--- src/reducers/tareasReducer.js
+++ src/reducers/tareasReducer.js
@@ -3,7 +3,9 @@ import { TRAER_TODAS, CARGANDO, ERROR } from "../types/tareasTypes";
 const INITIAL_STATE = {
     tareas: {},
     cargando: false,
-    error: ''
+    error: '',
+    usuario_id: '',
+    titulo: ''
 };
 
 export default (state = INITIAL_STATE, action) => {
@@ -28,6 +30,18 @@ export default (state = INITIAL_STATE, action) => {
                 error: action.payload,
                 cargando: false
             };
+
+        case 'cambio_usuario_id':
+            return {
+                ...state,
+                usuario_id: action.payload
+            }
+
+        case 'cambio_titulo':
+            return {
+                ...state,
+                titulo: action.payload
+            }
 
         default:
             return state;
